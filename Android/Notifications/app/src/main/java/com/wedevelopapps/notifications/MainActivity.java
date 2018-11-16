7901e819543e384dbc930d36a4faabe633cfff9d
Ahraar <ahraralam2012@gmail.com>
2018-11-16 14:26:16 +0530

    Notification

--- Android/Notifications/app/src/main/java/com/wedevelopapps/notifications/MainActivity.java
+++ Android/Notifications/app/src/main/java/com/wedevelopapps/notifications/MainActivity.java
@@ -1,13 +1,54 @@
 package com.wedevelopapps.notifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    //1. Notification Channel
+    //2. Notification Builder
+    //3. Notification Manager
+
+    //TODO Notification Channel
+    private static final String CHANNEL_ID = "CHATX";
+    private static final String CHANNEL_NAME = "CHATX";
+    private static final String CHANNEL_DESC = "CHATX Notification";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //for Oreo and above version we have to add this
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        findViewById(R.id.buttonNotify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification();
+            }
+        });
+    }
+
+    private void displayNotification() {
+        //TODO Notification Builder
+        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.app_icon)
+                .setContentTitle("Hurray! Is is working...")
+                .setContentText("My first message")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        //TODO Notification Manager
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(1, nBuilder.build());
     }
 }
