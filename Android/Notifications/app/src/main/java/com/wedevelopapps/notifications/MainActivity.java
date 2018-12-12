bec4aad4db45b75327597e9efb8faeaf73ff1970
Ahraar <ahraralam2012@gmail.com>
2018-12-12 16:11:39 +0530

    Device Token Practice

--- Android/Notifications/app/src/main/java/com/wedevelopapps/notifications/MainActivity.java
+++ Android/Notifications/app/src/main/java/com/wedevelopapps/notifications/MainActivity.java
@@ -3,11 +3,18 @@ package com.wedevelopapps.notifications;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
     //1. Notification Channel
@@ -18,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String CHANNEL_ID = "CHATX";
     private static final String CHANNEL_NAME = "CHATX";
     private static final String CHANNEL_DESC = "CHATX Notification";
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +41,21 @@ public class MainActivity extends AppCompatActivity {
             manager.createNotificationChannel(channel);
         }
 
-        findViewById(R.id.buttonNotify).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayNotification();
-            }
-        });
+        textView = findViewById(R.id.TextViewToken);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+
+                        if (task.isSuccessful()) {
+                            String token = task.getResult().getToken();
+                            textView.setText("Token : " + token);
+                        } else {
+                            textView.setText(task.getException().getMessage());
+                        }
+                    }
+                });
+
     }
 
     private void displayNotification() {
