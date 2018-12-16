f5e33df58c63ca3555c98dcd40b3d84806d3f88b
Ahraar <ahraralam2012@gmail.com>
2018-12-16 15:08:24 +0530

    Models

--- Android/Retrofit/app/src/main/java/com/wedevelopapps/retrofit/View/MainActivity.java
+++ Android/Retrofit/app/src/main/java/com/wedevelopapps/retrofit/View/MainActivity.java
@@ -1,7 +1,9 @@
-package com.wedevelopapps.retrofit;
+package com.wedevelopapps.retrofit.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wedevelopapps.retrofit.R;
 
 public class MainActivity extends AppCompatActivity {
 
